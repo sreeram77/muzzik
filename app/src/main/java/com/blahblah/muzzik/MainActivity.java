5a0797148d1bf2b81868fdce57ad6d6faa8892d1
Sreeram Narayanan <sreeram.an1994@gmail.com>
2019-04-02 20:49:59 +0530

    Music player base built

--- app/src/main/java/com/blahblah/muzzik/MainActivity.java
+++ app/src/main/java/com/blahblah/muzzik/MainActivity.java
@@ -1,13 +1,49 @@
 package com.blahblah.muzzik;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+
+    Muzzik muzzik = new Muzzik();
+    Uri currentSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("audio/*");
+        startActivityForResult(intent,1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK){
+                currentSong = data.getData();
+                Log.e("BLAH", currentSong.toString());
+                try {
+                    muzzik.setDataSource(getApplicationContext(), currentSong);
+                } catch (IOException e){
+                    Log.e("Error", e.toString());
+                }
+                try {
+                    muzzik.prepare();
+                } catch (IOException e){
+                    Log.e("Error", e.toString());
+                }
+                muzzik.start();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/blahblah/muzzik/Muzzik.java
@@ -0,0 +1,71 @@
+package com.blahblah.muzzik;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.MediaTimestamp;
+import android.net.Uri;
+
+import java.io.IOException;
+
+public class Muzzik extends MediaPlayer {
+    @Override
+    public void setDataSource(@androidx.annotation.NonNull Context context, @androidx.annotation.NonNull Uri uri) throws IOException, IllegalArgumentException, IllegalStateException, SecurityException {
+        super.setDataSource(context, uri);
+    }
+
+    @Override
+    public void prepare() throws IOException, IllegalStateException {
+        super.prepare();
+    }
+
+    @Override
+    public void prepareAsync() throws IllegalStateException {
+        super.prepareAsync();
+    }
+
+    @Override
+    public void start() throws IllegalStateException {
+        super.start();
+    }
+
+    @Override
+    public void stop() throws IllegalStateException {
+        super.stop();
+    }
+
+    @Override
+    public void pause() throws IllegalStateException {
+        super.pause();
+    }
+
+    @Override
+    public void seekTo(int msec) throws IllegalStateException {
+        super.seekTo(msec);
+    }
+
+    @androidx.annotation.Nullable
+    @Override
+    public MediaTimestamp getTimestamp() {
+        return super.getTimestamp();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return super.getCurrentPosition();
+    }
+
+    @Override
+    public int getDuration() {
+        return super.getDuration();
+    }
+
+    @Override
+    public void release() {
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        super.reset();
+    }
+}
